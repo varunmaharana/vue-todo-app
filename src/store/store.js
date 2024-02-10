88cf091c75d4e069a71b7ade4fa1ce281c00a1e0
varunmaharana <varunmaharana14@gmail.com>
2024-02-10 10:40:13 +0530

    minor fix

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import { createStore } from 'vuex'
 const todoListModule = {
   state() {
     return {
-      todoList: localStorage.getItem('todoList')?.split(',') || []
+      todoList: JSON.parse(localStorage.getItem('todoList')) || []
     }
   },
   getters: {
@@ -16,7 +16,7 @@ const todoListModule = {
       if (payload.length > 0) {
         state.todoList.push(payload)
         // saving to localStorage
-        localStorage.setItem('todoList', state.todoList)
+        localStorage.setItem('todoList', JSON.stringify(state.todoList))
       } else {
         alert('Cant save empty todo!')
       }
@@ -28,14 +28,14 @@ const todoListModule = {
       //   state.todoList.remove(payload)
       state.todoList = updatedList
       // saving to localStorage
-      localStorage.setItem('todoList', updatedList)
+      localStorage.setItem('todoList', JSON.stringify(updatedList))
     },
     editTodo(state, payload) {
       const [updatedTodo, index] = payload
       state.todoList[index] = updatedTodo
       // saving to localStorage
       console.log('edit')
-      localStorage.setItem('todoList', state.todoList)
+      localStorage.setItem('todoList', JSON.stringify(state.todoList))
     }
   },
   actions: {
